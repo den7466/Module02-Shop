1d177d77d61051af6107f3db789c3bfefb8c5d5a
den7466 <veseliy.koteika@yandex.ru>
2022-03-25 09:47:25 +0300

    Fix js for mobile menu

---
+++ dist/images/68313cccfb86aab9a422.js
@@ -0,0 +1,1 @@
+function openMobMenu(){document.getElementById("mob-menu").style.margin="0"}function closeMobMenu(){document.getElementById("mob-menu").style.margin="-450px 0 0 0"}
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Mobile-Menu
+function openMobMenu() {
+    document.getElementById("mob-menu").style.margin = "0";
+}
+
+function closeMobMenu() {
+    document.getElementById("mob-menu").style.margin = "-450px 0 0 0";
+}
